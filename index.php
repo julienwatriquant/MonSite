c39a0092ba09ef855a2ffdd5d5edf810c880e6ad
Julien Watriquant <julienwatriquant01@gmail.com$ git config --global user.email julienwatriquant01@gmail.com>
2020-01-07 09:42:58 +0100

    ajout paragraphe

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 
    <h1 class="display-4 text-center text-info"> Decouverte GIT / GITHUB</h1> 
 
+<p class="col-md-6 mx-auto alert alert-info text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolores libero, quod distinctio laudantium non id reiciendis, vel quas, aperiam quibusdam accusantium repudiandae doloribus laborum! Quod eligendi esse ab magnam.</p>
+
 
    </div>
 </body>
